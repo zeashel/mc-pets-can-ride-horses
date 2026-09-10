37f6acbadae6f88a0a7097d365c54db3d3cb84ab
zeashel <182934048+zeashel@users.noreply.github.com>
2026-09-11 02:37:17 +0700

    Add symlink script
    - creates a symlink of the data and resource pack folder into the macos minecraft directory
    - also appends the two directory sources into minecraft's allowed_symlinks.txt

---
+++ symlink.sh
@@ -0,0 +1,22 @@
+# create symlinks to selected world's datapack folder and global resourcepack folder
+#
+# macos only sorry
+# usage: ./symlink.sh <YOUR TESTING WORLD>
+# eg: ./symlink.sh "Datapack Testing"
+
+MCPATH="$HOME/Library/Application Support/minecraft"
+DATA="pets_can_ride_horses"
+RESOURCE="invisible_acacia_boat"
+
+if [ "$#" -eq 1 ]; then
+    # datapack
+    ln -siv "$PWD/$DATA" "$MCPATH/saves/$1/datapacks"
+
+    # resourcepack
+    ln -siv "$PWD/$RESOURCE" "$MCPATH/resourcepacks"
+fi
+
+# allow minecraft symlinks for only these directories:
+touch "$MCPATH/allowed_symlinks.txt" 
+echo "$PWD/$DATA" >> "$MCPATH/allowed_symlinks.txt"  
+echo "$PWD/$RESOURCE" >> "$MCPATH/allowed_symlinks.txt"  
