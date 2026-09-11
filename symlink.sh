375eed2102b818fc7ac694ab8918c06068972fd8
zeashel <182934048+zeashel@users.noreply.github.com>
2026-09-11 21:14:00 +0700

    Add error message to symlink.sh, update constants

--- symlink.sh
+++ symlink.sh
@@ -7,22 +7,27 @@
 # eg: ./symlink.sh "Datapack Testing"
 
 readonly MCPATH="$HOME/Library/Application Support/minecraft"
-readonly DATA="pets_can_ride_horses"
-readonly RESOURCE="invisible_acacia_boat"
+readonly DATA_PACK="pets_can_ride_horses"
+readonly RESOURCE_PACK="invisible_acacia_boat"
 
 if [ -d "$MCPATH" ]; then
 
     # allow minecraft symlinks for only these directories:
     touch "$MCPATH/allowed_symlinks.txt" 
-    echo "$PWD/$DATA" >> "$MCPATH/allowed_symlinks.txt"  
-    echo "$PWD/$RESOURCE" >> "$MCPATH/allowed_symlinks.txt"  
+    echo "$PWD/$DATA_PACK" >> "$MCPATH/allowed_symlinks.txt"  
+    echo "$PWD/$RESOURCE_PACK" >> "$MCPATH/allowed_symlinks.txt"  
 
     if [ "$#" -eq 1 ]; then
         # datapack
-        ln -siv "$PWD/$DATA" "$MCPATH/saves/$1/datapacks"
+        ln -siv "$PWD/$DATA_PACK" "$MCPATH/saves/$1/datapacks"
 
         # resourcepack
-        ln -siv "$PWD/$RESOURCE" "$MCPATH/resourcepacks"
+        ln -siv "$PWD/$RESOURCE_PACK" "$MCPATH/resourcepacks"
+    else
+        printf "%s\n%s\n%s" \
+            "$0: world name not specified." \
+            "usage: $0 <WORLD NAME>" \
+            "example: $0 \"Datapack Testing\""
     fi
 
 else
